d75b93f910f8ad34e7bead547d28aaed762884e3
CellEvolution <53360913+CellEvolution@users.noreply.github.com>
2019-08-01 20:26:18 +0800

    Delete minmod.m

--- example1/minmod.m
+++
@@ -1,9 +0,0 @@
-% This code is the minmod function used in the slope limiter
-% zhiwen zhang
-% 20130513
-
-function res = minmod(a,b)
-
-%% parameter 
-%% compute
-res = 0.5*(sign(a) + sign(b)).*min(abs(a),abs(b));
